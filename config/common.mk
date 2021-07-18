25d69dd70a48a760ddcfdf01cacc980c679e5c30
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-07-18 16:12:46 +0000

    vendor: Build StitchImage

--- config/common.mk
+++ config/common.mk
@@ -257,6 +257,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/havoc/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
 
+# Long Screenshot
+PRODUCT_PACKAGES += \
+    StitchImage
+
 # Enable ccache
 USE_CCACHE := true
 
